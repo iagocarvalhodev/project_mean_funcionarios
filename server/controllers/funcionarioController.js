7cafd3f954c3408a8209211e901964bb5728463d
Iago Carvalho <iagocarvalhoweb@gmail.com>
2018-11-07 21:00:41 -0200

    Add Method Show e Update

--- server/controllers/funcionarioController.js
+++ server/controllers/funcionarioController.js
@@ -2,28 +2,40 @@ const Funcionario = require('../models/funcionario');
 
 const funcionarioController = {};
 
+// trazer todos os funcionarios
 funcionarioController.getFuncionarios = async (req, res) => {
     const funcionarios = await Funcionario.find();
     res.json(funcionarios);
 }
 
+// criar um novo funcionario
 funcionarioController.createFuncionario = async (req, res) =>  {
     const funcionario = new Funcionario(req.body);
     await funcionario.save();
     res.json(funcionario);
 }
 
-funcionarioController.showFuncionario = (req, res) => {
-    res.json({
-        status: 'Mostrar usuario com a id'
-    })
+// trazer as informações de um funcionario especifico pelo id
+funcionarioController.showFuncionario = async (req, res) => {
+    const funcionario = await Funcionario.findById(req.params.id);
+    res.json({ funcionario });
 }
 
-funcionarioController.updateFuncionario = (req, res) => {
+// atualizar informações do funcionario buscando ele pelo id
+funcionarioController.updateFuncionario = async (req, res) => {
+    const { id } = req.params;
+    const funcionario = {
+        name: req.body.name,
+        position: req.body.position,
+        office: req.body.office,
+        salary: req.body.salary
+    }
 
+    funcionarioupdate = Funcionario.findByIdAndUpdate(id, {$set: funcionario}, {new: true});
+    res.json({ funcionarioupdate });
 }
 
-funcionarioController.deleteFuncionario = (req, res) => {
+funcionarioController.deleteFuncionario = async (req, res) => {
 
 }
 

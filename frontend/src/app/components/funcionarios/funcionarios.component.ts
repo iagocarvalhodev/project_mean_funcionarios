7c8e4496d61dddea007d6ac0d4b75c21da39a2cb
Iago Carvalho <iagocarvalhoweb@gmail.com>
2018-11-08 13:38:50 -0200

    Add function Add Funcionario is reset form

--- frontend/src/app/components/funcionarios/funcionarios.component.ts
+++ frontend/src/app/components/funcionarios/funcionarios.component.ts
@@ -1,5 +1,10 @@
 import { FuncionarioService } from './../../services/funcionario.service';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Funcionario } from 'src/app/models/funcionario';
+
+
+declare var M: any;
 
 
 @Component({
@@ -14,4 +19,18 @@ export class FuncionariosComponent implements OnInit {
   ngOnInit() {
   }
 
+  addFuncionario(form: NgForm){
+    this.funcionarioService.postFuncionario(form.value)
+      .subscribe(res => {
+        this.resetForm(form);
+        M.toast({ html: 'Funcionario Registrado' });
+      });
+  }
+
+  resetForm(form?: NgForm) {
+    if(form) {
+      form.reset();
+      this.funcionarioService.selectedFuncionario = new Funcionario;
+    }
+  }
 }

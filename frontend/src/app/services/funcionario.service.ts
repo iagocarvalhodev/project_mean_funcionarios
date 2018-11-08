4078a1120ebfc88723066be3341f9e5b209d4ac3
Iago Carvalho <iagocarvalhoweb@gmail.com>
2018-11-07 23:39:39 -0200

    Created Service Funcionario

---
+++ frontend/src/app/services/funcionario.service.ts
@@ -0,0 +1,29 @@
+import { Funcionario } from './../models/funcionario';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class FuncionarioService {
+
+  selectedFuncionario: Funcionario;
+
+  readonly URL_API = 'http://localhost:3000/api/funcionarios';
+
+  constructor(private http: HttpClient) { }
+
+
+  getFuncionarios() {
+    return this.http.get(this.URL_API);
+  }
+
+  postFuncionario(funcionario: Funcionario) {
+    return this.http.post(this.URL_API, funcionario);
+  }
+
+  putFuncionario(funcionario: Funcionario) {
+    return this.http.put(this.URL_API + `/${funcionario._id}`, funcionario);
+  }
+
+
+}
